2012dac25429f0143c6ac2b81af58346a87f8af5
MsYoda <kostga123@mail.ru>
2020-01-04 14:09:19 +0300

    Загружены основные файлы

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    private PhotonView photonView;
+
+	// Use this for initialization
+	void Start () {
+        photonView = GetComponent<PhotonView>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!photonView.IsMine) return;
+
+		if ( Input.GetKey( KeyCode.LeftArrow ) )
+        {
+            transform.Translate( -Time.deltaTime * 5, 0, 0 );
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Time.deltaTime * 5, 0, 0);
+        }
+    }
+}
